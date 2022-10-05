ea6f82324092ad1848526be47ddeb944fca5d1e7
Molla.Al <hope.miky1074@gmail.com>
2022-10-05 21:49:51 +0200

    working on auth and customer api view test

--- user/models.py
+++ user/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, Group, Permission
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.base_user import AbstractBaseUser as AbstractBaseUser
 
@@ -38,23 +38,37 @@ class CustomerManager(BaseUserManager):
 
     use_in_migrations = True
 
-    def _create_user(self, email, password, **extra_fields):
+    def _create_customer(self, email, password, **extra_fields):
         if not email:
             raise ValueError('The given email must be set')
         user = self.model(email=email, username=email, **extra_fields)
         user.set_password(password)
-        user.save(using=self._db)
+        # user.save(using=self._db)
         return user
 
-    def create_user(self, email, password, **extra_fields):
+    def create_customer(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, **extra_fields)
+        return self._create_customer(email, password, **extra_fields)
 
 
-class Customer(AbstractBaseUser):
+class Customer(AbstractUser):
     email  = models.EmailField(unique=True, max_length=30)
     username = models.CharField(unique=True, max_length=30)
+
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name='groups',
+        blank=True,
+        related_name="customers_set",
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name='user permissions',
+        blank=True,
+        related_name="customers_set",
+    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
